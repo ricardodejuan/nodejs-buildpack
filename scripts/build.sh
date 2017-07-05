cbd91dcef74f2867080d8694f435e2c7f0d04604
de Juan Cajide <jddn@nbmad870.local>
2017-07-04 21:02:44 -0400

    Little changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,6 @@ ROOTDIR="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )"
 BINDIR=$ROOTDIR/bin
 
 export GOPATH=$ROOTDIR
-echo "aa"
 export GOOS=linux
 
 go build -o $BINDIR/supply nodejs/supply/cli
