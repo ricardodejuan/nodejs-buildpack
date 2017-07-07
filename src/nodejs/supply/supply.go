5dcec1320092700443671eb6d51737256d8c9738
de Juan Cajide <jddn@nbmad870.local>
2017-07-06 22:44:42 -0400

    Little changes

--- src/nodejs/supply/supply.go
+++ src/nodejs/supply/supply.go
@@ -253,7 +253,7 @@ export NODE_ENV=${NODE_ENV:-production}
 export MEMORY_AVAILABLE=$(echo $VCAP_APPLICATION | jq '.limits.mem')
 export WEB_MEMORY=512
 export WEB_CONCURRENCY=1
-export PATH=$(echo $PATH):/home/vcap/app/.java/bin
+export PATH=$(echo $PATH):$(echo $JAVA_HOME)
 `
 
 	return s.Stager.WriteProfileD("node.sh", fmt.Sprintf(scriptContents, filepath.Join("$DEPS_DIR", s.Stager.DepsIdx(), "node")))
